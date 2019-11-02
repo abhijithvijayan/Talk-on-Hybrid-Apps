7d07c539850640d6f81c8ce734c7c9793ce8343e
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-11-02 13:27:19 +0530

    chore: minor tweaks

--- packages/slides/src/index.js
+++ packages/slides/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import { AppContainer } from 'react-hot-loader';
 import Redbox from 'redbox-react';
 
@@ -18,18 +18,19 @@ ReactDOM.render(
     <AppContainer errorReporter={CustomErrorReporter}>
         <Presentation />
     </AppContainer>,
-    document.getElementById('root')
+    document.getElementById('app')
 );
 
 if (module.hot) {
     module.hot.accept('./presentation', () => {
         // eslint-disable-next-line global-require
         const NextPresentation = require('./presentation').default;
+
         ReactDOM.render(
             <AppContainer errorReporter={CustomErrorReporter}>
                 <NextPresentation />
             </AppContainer>,
-            document.getElementById('root')
+            document.getElementById('app')
         );
     });
 }
