6e22fafc3cf2b7dac667df01e7e2c5633c383a2f
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-11-06 22:42:49 +0530

    fix: all sorts of fixes

--- packages/slides/src/components/Slide1.js
+++ packages/slides/src/components/Slide1.js
@@ -3,8 +3,24 @@ import styled from 'styled-components';
 import { Heading, Layout, Image, Fill, Fit, Text, Slide } from 'spectacle';
 
 import ProImage from '../assets/images/profile.jpg';
+import DevicesImage from '../assets/images/devices.png';
+
+const StyledHeader = styled(Heading)`
+    text-align: center;
+    font-size: 2.7rem !important;
+`;
 
 const StyledText = styled(Text)`
+    font-size: 2rem !important;
+    font-weight: ${({ theme }) => {
+        return theme.medium;
+    }};
+    font-family: ${({ theme }) => {
+        return theme.railway;
+    }} !important;
+`;
+
+const StyledSecondaryText = styled(Text)`
     font-weight: ${({ theme }) => {
         return theme.medium;
     }};
@@ -15,19 +31,32 @@ const StyledText = styled(Text)`
 
 const Slide1 = props => {
     return (
-        <Slide transition={['zoom']} bgColor="lightBlue" {...props}>
-            <Heading size={1} fit caps lineHeight={2} textColor="primary">
-                Hybrid Apps
-            </Heading>
+        <Slide transition={['zoom']} bgColor="primary" {...props}>
+            <Image
+                src={DevicesImage}
+                style={{
+                    height: '8em',
+                    width: 'auto',
+                    margin: 0,
+                    display: 'inline-block',
+                }}
+            />
+            <StyledHeader style={{}} lineHeight={4} textColor="dark">
+                Hybrid Multi-platform / Cross-platform Apps
+            </StyledHeader>
             <Layout style={{ justifyContent: 'center', marginBottom: '5em' }}>
-                <Fill style={{ maxWidth: '68%' }}>
-                    <StyledText style={{ textAlign: 'left' }} textColor="lightWhite">
+                <Fill style={{ maxWidth: '48%' }}>
+                    <StyledText style={{ textAlign: 'left' }} textColor="secondary">
                         The Art of
                     </StyledText>
-                    <StyledText textColor="yellow" margin={10} style={{ paddingLeft: '1em' }}>
+                    <StyledText
+                        textColor="secondary"
+                        margin={10}
+                        style={{ textDecoration: 'underline', paddingLeft: '0.7em' }}
+                    >
                         Being in All Places
                     </StyledText>
-                    <StyledText style={{ textAlign: 'right' }} textColor="lightWhite">
+                    <StyledText style={{ textAlign: 'right' }} textColor="secondary">
                         at Once!
                     </StyledText>
                 </Fill>
@@ -53,25 +82,25 @@ const Slide1 = props => {
                             display: 'inline-block',
                         }}
                     />
-                    <StyledText
-                        textColor="primary"
+                    <StyledSecondaryText
+                        textColor="black"
                         style={{ margin: 'auto', marginLeft: '10px !important', fontSize: '22px' }}
                     >
                         Abhijith Vijayan
-                    </StyledText>
+                    </StyledSecondaryText>
                     <Fit>
-                        <StyledText
-                            textColor="primary"
+                        <StyledSecondaryText
+                            textColor="black"
                             style={{ margin: 'auto', marginRight: '0px', fontSize: '20px', paddingBottom: '5px' }}
                         >
                             {`${new Date().getDay()}-${new Date().getMonth() + 1}-${new Date().getFullYear()}`}
-                        </StyledText>
-                        <StyledText
-                            textColor="primary"
+                        </StyledSecondaryText>
+                        <StyledSecondaryText
+                            textColor="black"
                             style={{ margin: 'auto', marginRight: '55px', fontSize: '20px', paddingTop: '5px' }}
                         >
                             @abhijithvijayan
-                        </StyledText>
+                        </StyledSecondaryText>
                     </Fit>
                 </Fill>
             </Layout>
